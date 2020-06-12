bbbd4b60efb942894c67be1fb47b4a519a4d18b7
imartinezl <inigomlap@gmail.com>
2020-06-12 08:51:39 +0200

    tensorflow model

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ from wtforms import FloatField, SubmitField
 from wtforms.validators import InputRequired, DataRequired
 import tensorflow as tf
 print(tf.__version__)
+model = tf.keras.models.load_model('my_model')
+print(model.summary())
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess my string'
@@ -40,6 +42,11 @@ def index():
         value_format= "{:.2f}".format(form.valeur1.data / form.valeur2.data)
         session['result'] = value_format
         return redirect(url_for('index'))
+    example_texts = ["This was an absolutely terrible movie. Don't be lured in by Christopher Walken or Michael Ironside. Both are great actors, but this must simply be their worst role in history. Even their great acting could not redeem this movie's ridiculous storyline. This movie is an early nineties US propaganda piece. The most pathetic scenes were those when the Columbian rebels were making their cases for revolutions. Maria Conchita Alonso appeared phony, and her pseudo-love affair with Walken was nothing but a pathetic emotional plug in a movie that was devoid of any real meaning. I am disappointed that there are movies like this, ruining actor's like Christopher Walken's good name. I could barely sit through it.",
+    "This is the kind of film for a snowy Sunday afternoon when the rest of the world can go ahead with its own business as you descend into a big arm-chair and mellow for a couple of hours. Wonderful performances from Cher and Nicolas Cage (as always) gently row the plot along. There are no rapids to cross, no dangerous waters, just a warm and witty paddle through New York life at its best. A family film in every sense and one that deserves the praise it received."]
+    result = model.predict(example_texts)
+    print(result)
+
     return render_template('index.html', form=form, result=session.get('result'))
 
 
